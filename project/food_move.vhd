e58313737dfcb96d4c3ccb436984a6620fada273
Hagai Kariti <hkariti@gmail.com>
2018-06-16 11:51:43 +0300

    more

--- project/food_move.vhd
+++ project/food_move.vhd
@@ -11,29 +11,28 @@ port 	(
 		CLK				: in std_logic; --						//	27 MHz
 		RESETn			: in std_logic; --			//	50 MHz
 		timer_done		: in std_logic;
-		StartX         : in std_logic_vector(31 downto 0);
+		random          : in integer;
 		collision		: in std_logic;
-		ObjectStartX	: out std_logic_vector(31 downto 0) ;
-		ObjectStartY	: out std_logic_vector(31 downto 0)
-		
+		ObjectStartX	: out integer ;
+		ObjectStartY	: out integer
 	);
 end;
 
 architecture behav of food_move is 
 
 constant StartY : integer := 480;   -- starting point
-constant marginY: integer := 128;
-constant marginX: integer := 600;
 constant baseStartX: integer := 400;
-constant speed: integer := 1; 
+constant marginY: integer := 128;   -- allowed deviation from starting point
+constant marginX: integer := 600;
+constant speed: integer := 1;       -- speed of the object
 
-signal ObjectStartX_t : integer; -- range 0 to 640;  --vga screen size 
-signal ObjectStartY_t : integer; -- range 80 to 400;
+signal ObjectStartX_t : integer;
+signal ObjectStartY_t : integer;
 
 begin
 		process ( RESETn,CLK)
-        variable recalc_Y : integer := StartY + (to_integer(unsigned(startX)) mod marginY);
-        variable recalc_X : integer := (to_integer(unsigned(StartX)) mod marginX) + baseStartX;
+        variable recalc_Y : integer := StartY + (random mod marginY);
+        variable recalc_X : integer := (random mod marginX) + baseStartX;
 		begin
 		  if RESETn = '0' then
 				ObjectStartY_t	<= recalc_Y;
@@ -54,7 +53,7 @@ begin
 				end if;	
 			end if;
 		end process;
-ObjectStartX	<= std_logic_vector(to_unsigned(ObjectStartX_t, 32));		-- copy to outputs 	
-ObjectStartY	<= std_logic_vector(to_unsigned(ObjectStartY_t, 32));	
+ObjectStartX	<= ObjectStartX_t;		-- copy to outputs 	
+ObjectStartY	<= ObjectStartY_t;	
 
 end behav;
